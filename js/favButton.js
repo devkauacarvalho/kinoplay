2a4cd9772433c865ebf1e5d9a233554629001baf
Kauã Carvalho <contato.kauacarval@gmail.com>
2023-01-19 16:20:22 -0300

    manipulando a função de botões de fav

---
+++ js/favButton.js
@@ -0,0 +1,17 @@
+const heartBtns = document.querySelectorAll('.heartBtn');
+let favoritos = [];
+
+for (let i = 0; i < heartBtns.length; i++) {
+  heartBtns[i].addEventListener('click', function() {
+    this.classList.toggle('ri-heart-add-line');
+    this.classList.toggle('ri-heart-fill');
+    if (this.classList.contains('ri-heart-fill')) {
+      favoritos.push(this.closest('div').innerHTML);
+      localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    } else {
+      // remover o item do array
+      favoritos.splice(favoritos.indexOf(this.closest('div').innerHTML), 1);
+      localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    }
+  });
+}
